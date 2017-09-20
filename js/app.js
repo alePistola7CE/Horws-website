42f977af2747d16cca7386fa83aaa20230fa1365
Alessandro Pistola <pistola.alessandro@virgilio.it>
2017-09-20 21:42:04 +0200

    mobile view added

--- js/app.js
+++ js/app.js
@@ -2,16 +2,38 @@ $(function() {
 
     //Set up instafeed
     var feed = new Instafeed({
-        accessToken: '4506821562.1677ed0.ed6e9447b7594b5b810127e23dd0770b',
+        clientId: '404795e9bb394bf3bdfdf4e5684c908b',
+        accessToken: '4506821562.404795e.21136a7408dd426d9e76d98e97b36e69',
         target: 'instafeed',
         get: 'tagged',
-        tagName: 'dolomiti',
+        tagName: '1',
         links: true,
-        limit: 8,
-        sortBy: 'most-recent',
+        limit: 6,
+        sortBy: 'random',
         resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
     });
+
     feed.run();
+
+  /*  //Set up instafeed
+    var feed2 = new Instafeed({
+        clientId: '404795e9bb394bf3bdfdf4e5684c908b',
+        accessToken: '4506821562.404795e.21136a7408dd426d9e76d98e97b36e69',
+        target: 'instafeed2',
+        get: 'tagged',
+        tagName: '2',
+        links: true,
+        limit: 3,
+        sortBy: 'random',
+        resolution: 'standard_resolution',
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+    });
+
+    feed2.run();*/
+
+
+
+
 
 });
