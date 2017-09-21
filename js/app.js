d4b5bb7c9cf3aaa237dc14889b23e1ac2eed01ce
Alessandro Pistola <pistola.alessandro@virgilio.it>
2017-09-21 19:15:19 +0200

    animation fix

--- js/app.js
+++ js/app.js
@@ -40,25 +40,4 @@ $(function() {
 
     feed.run();
 
-
-  /*  //Set up instafeed
-    var feed2 = new Instafeed({
-        clientId: '404795e9bb394bf3bdfdf4e5684c908b',
-        accessToken: '4506821562.404795e.21136a7408dd426d9e76d98e97b36e69',
-        target: 'instafeed2',
-        get: 'tagged',
-        tagName: '2',
-        links: true,
-        limit: 3,
-        sortBy: 'random',
-        resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
-    });
-
-    feed2.run();*/
-
-
-
-
-
 });
