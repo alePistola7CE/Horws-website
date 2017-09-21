73bb9cdfe620794d202837f8ac70bbae93e7ac00
Alessandro Pistola <pistola.alessandro@virgilio.it>
2017-09-21 18:12:50 +0200

    modal view optimization

--- js/app.js
+++ js/app.js
@@ -7,14 +7,39 @@ $(function() {
         target: 'instafeed',
         get: 'tagged',
         tagName: '1',
-        links: true,
+        links: false,
         limit: 6,
         sortBy: 'random',
         resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
+        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><img src="{{image}}" class="photo" alt="{{caption}}"><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>',
+        after: function(){
+          var modal = document.getElementById('myModal');
+          var modalImg = document.getElementById("img01");
+          var captionText = document.getElementById("caption");
+          var instafeed = document.getElementById('instafeed');
+          for (i = 1; i < instafeed.childElementCount; i++) {
+            var div = instafeed.children[i];
+            var photobox = div.children[0];
+            var imagewrap = photobox.children[0];
+            //var a = imagewrap.children[0];
+            var img = imagewrap.children[0];
+            img.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+            }
+          }
+
+          var span = document.getElementsByClassName("close")[0];
+          span.onclick = function() {
+          modal.style.display = "none";
+          }
+
+        }
     });
 
     feed.run();
+
 
   /*  //Set up instafeed
     var feed2 = new Instafeed({
