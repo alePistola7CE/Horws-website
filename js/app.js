16dcac9df4c39df9b80fed125d8428006c32d566
Alessandro Pistola <pistola.alessandro@virgilio.it>
2017-09-23 08:35:21 +0200

    New Top icon feauture

--- js/app.js
+++ js/app.js
@@ -40,4 +40,22 @@ $(function() {
 
     feed.run();
 
+
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0) {
+                        //se non siamo in cima alla pagina
+            $('#top').fadeIn(); //faccio apparire il box
+        } else {
+                        //altrimenti (il visitatore è in cima alla pagina scrollTop = 0)
+            $('#top').fadeOut();//Il box scompare
+        }
+    });//Allo scroll function
+
+
+    $('#top').click(function() {
+                //Se clicco sul box torno su (scrollTop:0) con un timing di animazione.
+        $('body,html').animate({scrollTop:0},800);
+    });//Click
+
 });
