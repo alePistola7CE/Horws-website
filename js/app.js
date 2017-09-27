4e62cb6b208ad87854a7e4ad42910ba5cac86d6e
Alessandro Pistola <pistola.alessandro@virgilio.it>
2017-09-27 20:36:19 +0200

    Added photos.html and loadmore button

--- js/app.js
+++ js/app.js
@@ -1,46 +1,110 @@
 $(function() {
 
     //Set up instafeed
-    var feed = new Instafeed({
-        clientId: '404795e9bb394bf3bdfdf4e5684c908b',
-        accessToken: '4506821562.404795e.21136a7408dd426d9e76d98e97b36e69',
-        target: 'instafeed',
-        get: 'tagged',
-        tagName: '1',
-        links: false,
-        limit: 6,
-        sortBy: 'random',
-        resolution: 'standard_resolution',
-        template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><img src="{{image}}" class="photo" alt="{{caption}}"><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>',
-        after: function(){
-          var modal = document.getElementById('myModal');
-          var modalImg = document.getElementById("img01");
-          var captionText = document.getElementById("caption");
-          var instafeed = document.getElementById('instafeed');
-          for (i = 1; i < instafeed.childElementCount; i++) {
-            var div = instafeed.children[i];
-            var photobox = div.children[0];
-            var imagewrap = photobox.children[0];
-            //var a = imagewrap.children[0];
-            var img = imagewrap.children[0];
-            img.onclick = function(){
-            modal.style.display = "block";
-            modalImg.src = this.src;
-            captionText.innerHTML = this.alt;
+    if (window.location.pathname == "/Horws-website/index.html"){
+      console.log("dentro index");
+      var feed = new Instafeed({
+          clientId: '404795e9bb394bf3bdfdf4e5684c908b',
+          accessToken: '4506821562.404795e.21136a7408dd426d9e76d98e97b36e69',
+          target: 'instafeed',
+          get: 'tagged',
+          tagName: '1',
+          links: false,
+          limit: 6,
+          sortBy: 'random',
+          resolution: 'standard_resolution',
+          template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><img src="{{image}}" class="photo" alt="{{caption}}"><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>',
+          after: function(){
+            var modal = document.getElementById('myModal');
+            var modalImg = document.getElementById("img01");
+            var captionText = document.getElementById("caption");
+            var instafeed = document.getElementById('instafeed');
+            for (i = 1; i < instafeed.childElementCount; i++) {
+              var div = instafeed.children[i];
+              var photobox = div.children[0];
+              var imagewrap = photobox.children[0];
+              //var a = imagewrap.children[0];
+              var img = imagewrap.children[0];
+              img.onclick = function(){
+              modal.style.display = "block";
+              modalImg.src = this.src;
+              captionText.innerHTML = this.alt;
+              }
+            }
+
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function() {
+            modal.style.display = "none";
             }
           }
-
-          var span = document.getElementsByClassName("close")[0];
-          span.onclick = function() {
-          modal.style.display = "none";
+      });
+      feed.run();
+    } else if(window.location.pathname == "/Horws-website/photos.html"){
+      console.log("dentro photos");
+      var feed2 = new Instafeed({
+          clientId: '404795e9bb394bf3bdfdf4e5684c908b',
+          accessToken: '4506821562.404795e.21136a7408dd426d9e76d98e97b36e69',
+          target: 'instafeed_all',
+          get: 'tagged',
+          tagName: '1',
+          links: false,
+          limit: 6,
+          sortBy: 'random',
+          resolution: 'standard_resolution',
+          template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4"><div class="photo-box"><div class="image-wrap"><img src="{{image}}" class="photo" alt="{{caption}}"><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>',
+          after: function(){
+            var modal = document.getElementById('myModal');
+            var modalImg = document.getElementById("img01");
+            var captionText = document.getElementById("caption");
+            var instafeed = document.getElementById('instafeed_all');
+            for (i = 1; i < instafeed.childElementCount; i++) {
+              var div = instafeed.children[i];
+              var photobox = div.children[0];
+              var imagewrap = photobox.children[0];
+              //var a = imagewrap.children[0];
+              var img = imagewrap.children[0];
+              img.onclick = function(){
+              modal.style.display = "block";
+              modalImg.src = this.src;
+              captionText.innerHTML = this.alt;
+              }
+            }
+            var moreButton = document.getElementById('more');
+            if (!this.hasNext()) {
+              moreButton.className = " not-allowed"
+              moreButton.style.opacity = 0.9;
+            }
+            moreButton.addEventListener('click', function() {
+                feed2.next();
+              })
           }
+      });
+      feed2.run();
+    }
 
-        }
-    });
+  /*  var slideIndex = 0;
+    showDivs(slideIndex);
 
-    feed.run();
+    function plusDivs(n) {
+      copriInstaAppenaCaricato();
+      showDivs(slideIndex += n);
+    }
 
+    function showDivs(n) {
+      var i;
+      var x = document.getElementsByClassName("slide");
+      if (n > x.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = x.length}
+      for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+      }
+      x[slideIndex-1].style.display = "block";
+      }
 
+      function copriInstaAppenaCaricato(){
+        instafeed = document.getElementById('instafeed_all');
+        instafeed.children[1].style.display = 'none';
+      }*/
 
     $(window).scroll(function() {
         if($(this).scrollTop() != 0) {
