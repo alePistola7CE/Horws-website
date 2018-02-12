72a7dedd918028a62a827511cc9e5928c270da96
alePistola7CE <pistola.alessandro@virgilio.it>
2018-02-12 22:28:38 +0100

    Added articles image view

--- wp-content/themes/horws-website/functions.php
+++ wp-content/themes/horws-website/functions.php
@@ -4,4 +4,8 @@
     add_theme_support('post-thumbnails');
   }
 
+  if ( function_exists( 'add_image_size' ) ) {
+	    add_image_size( 'homepage-thumb', 350, 252 ); 
+}
+
 ?>
--- wp-content/themes/horws-website/index.php
+++ wp-content/themes/horws-website/index.php
@@ -34,11 +34,41 @@
           <?php while(have_posts()) : the_post(); ?>
             <div class="instagram-content">
               <a class="titleLink" href="<?php the_permalink(); ?>">
-                <h3 id="Canazei"><?php the_title(); ?></h3>
+                <h3><?php the_title(); ?></h3>
               </a>
               <span class="descrizione">
                 <?php the_excerpt(); ?>
               </span>
+
+              <div>
+                <?php
+                  $attachments = get_posts( array(
+                    'post_type'   => 'attachment',
+                    'numberposts' => -1,
+                    'post_status' => null,
+                    'post_parent' => $post->ID
+                  ) );
+
+
+                  if ( $attachments ) {
+                    foreach ( $attachments as $attachment ) {
+                        ?>
+                        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                          <div class="photo-box">
+                            <div class="image-wrap">
+                              <?php echo wp_get_attachment_image( $attachment->ID, "homepage-thumb", "", array( "class" => "photo" ) ); ?>
+                              <div class="likes">12 Likes</div>
+                            </div>
+                            <div class="description">descrizione
+                              <div class="date">04/01/2019</div>
+                            </div>
+                          </div>
+                        </div>
+                        <?php
+                      }
+                    }
+                  ?>
+              </div>
 
               <div style="text-align: right; position: static; margin-right: 20px;">
                 See more about <a href="#<?php the_title(); ?>"><?php the_title(); ?></a>
@@ -50,6 +80,7 @@
           <p> <?php __('No post found'); ?></p>
         <?php endif; ?>
       </div>
+
     </div>
   </div>
 </section>
